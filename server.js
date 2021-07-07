14e41b2911b4ce27143f77f4846cf440cca2411e
TianleiZhu <50193937+TianleiZhu@users.noreply.github.com>
2021-07-07 19:07:09 +0800

    Update server.js

--- server.js
+++ server.js
@@ -19,22 +19,33 @@ var server = http.createServer(function(request, response){
 
   /******** 从这里开始看，上面不要看 ************/
 
-  console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
+  console.log('路径（带查询参数）为：' + pathWithQuery)
 
   if(path === '/'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`二哈`)
+    response.write(`
+    <!DOCTYPE html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="/x">
+    </head>
+    <body>
+        <h1>h1_tag</h1>  
+    </body>
+    </html>
+    `)
     response.end()
   } else if(path === '/x'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/css;charset=utf-8')
-    response.write(`body{color: red;}`)
-    response.end()
-  } else {
+    response.write(`
+    h1{color:red}
+    `)
+    response.end()}
+    else {
     response.statusCode = 404
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`你输入的路径不存在对应的内容`)
+    response.write(`你访问的页面不存在`)
     response.end()
   }
 
@@ -42,6 +53,4 @@ var server = http.createServer(function(request, response){
 })
 
 server.listen(port)
-console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
-
-
+console.log('监听 ' + port + ' 成功\n http://localhost:' + port)
